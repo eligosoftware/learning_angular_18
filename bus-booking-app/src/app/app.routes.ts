b1a332395071f06b87778f9b6799343f2815ea2b
Ilgar Rasulov <gragli98@gmail.com>
2025-12-08 16:25:02 +0100

    started with search component

--- bus-booking-app/src/app/app.routes.ts
+++ bus-booking-app/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Search } from './pages/search/search';
+import { Booking } from './pages/booking/booking';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'search',
+        pathMatch:'full'
+    },
+   {
+    path:'search',
+    component: Search
+   },
+   {
+    path:'booking',
+    component: Booking
+   }
+];
